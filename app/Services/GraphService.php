62a16a7231b2b207fbe272c21088bb700bf2878d
rizkychandra <rizkychandra2204@gmail.com>
2026-01-31 14:07:41 +0700

    update GraphService

--- app/Services/GraphService.php
+++ app/Services/GraphService.php
@@ -7,20 +7,35 @@ class GraphService
     public function getGraph()
     {
         return [
-            'A' => ['B' => 4, 'C' => 2],
-            'B' => ['A' => 4, 'C' => 1, 'D' => 5],
-            'C' => ['A' => 2, 'B' => 1, 'D' => 8],
-            'D' => ['B' => 5, 'C' => 8],
+            'rumah' => [
+                'kampus_ummi' => 4,
+                'secapa_polri' => 2
+            ],
+            'kampus_ummi' => [
+                'rumah' => 4,
+                'secapa_polri' => 1,
+                'cibadak' => 6
+            ],
+            'secapa_polri' => [
+                'rumah' => 2,
+                'kampus_ummi' => 1,
+                'cibadak' => 5
+            ],
+            'cibadak' => [
+                'kampus_ummi' => 6,
+                'secapa_polri' => 5
+            ]
         ];
     }
 
     public function getCoordinates()
     {
         return [
-            'A' => [-6.9741, 106.8246],
-            'B' => [-6.9750, 106.8252],
-            'C' => [-6.9738, 106.8239],
-            'D' => [-6.9760, 106.8260],
+            'rumah' => [-6.9083592252156905, 106.89633620913852],
+            'kampus_ummi' => [-6.9185548295720265, 106.93409279379514],
+            'secapa_polri' => [-6.911272035193913, 106.92435462263086],
+            'cibadak' => [-6.890065918562608, 106.78160176597994],
         ];
     }
+
 }
